95df9c21f74f08e243b1e24511f89e677ebd5312
DilmikaSahan <153601909+DilmikaSahan@users.noreply.github.com>
2026-01-21 13:30:22 +0530

    Implement HTTP WebClient and application.properties file configurations

---
+++ src/main/java/com/App/WeatherApp/Config/WebClientConfig.java
@@ -0,0 +1,34 @@
+package com.App.WeatherApp.Config;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+@Configuration
+public class WebClientConfig {
+    private static final int CONNECT_TIMEOUT = 5000; // 5 seconds
+    private static final int READ_TIMEOUT = 10; // 10 seconds
+    private static final int WRITE_TIMEOUT = 10; // 10 seconds
+
+    @Bean
+    public WebClient webClient() {
+        HttpClient httpClient = HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, CONNECT_TIMEOUT)
+                .responseTimeout(Duration.ofSeconds(15))
+                .doOnConnected(conn -> conn
+                        .addHandlerLast(new ReadTimeoutHandler(READ_TIMEOUT, TimeUnit.SECONDS))
+                        .addHandlerLast(new  WriteTimeoutHandler(WRITE_TIMEOUT, TimeUnit.SECONDS)));
+
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(httpClient))
+                .build();
+
+    }
+
+}
